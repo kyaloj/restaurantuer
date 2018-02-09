2ec3d81b3424a21d87ccfc2c012ca0d7997bd21d
Justus <mj.kyalo@gmail.com>
2018-02-09 11:01:42 +0300

    CORs

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,10 @@
-Rails.application.config.middleware.use Rack::Cors do
-    allow do
-        origins '*'
-        resource '*',
-        :headers => :any,
-        :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-        :methods => [:get, :post, :options, :delete, :put]
-    end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+	allow do
+	    origins '*'
+
+	    resource '*',
+	    headers: :any,
+	    expose:  ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+	    methods: [:get, :post, :put, :patch, :delete, :options, :head]
+	end
 end
